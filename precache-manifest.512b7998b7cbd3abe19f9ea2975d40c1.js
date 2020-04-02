34c7366c881534c4a2085247f7ed4e45411275cc
Patrick Cho <pck.cho@gmail.com>
2020-04-03 11:26:27 +1300

    Updates

---
+++ precache-manifest.512b7998b7cbd3abe19f9ea2975d40c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da0a0fcf35d2056726b6c6fe70781a28",
+    "url": "/celebrities-app/index.html"
+  },
+  {
+    "revision": "211c41b95da2ec562c75",
+    "url": "/celebrities-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7799ff80098a4143e2a9",
+    "url": "/celebrities-app/static/css/main.6d1e0305.chunk.css"
+  },
+  {
+    "revision": "211c41b95da2ec562c75",
+    "url": "/celebrities-app/static/js/2.d66e9514.chunk.js"
+  },
+  {
+    "revision": "7799ff80098a4143e2a9",
+    "url": "/celebrities-app/static/js/main.e5bc229d.chunk.js"
+  },
+  {
+    "revision": "83d413d9db7ea2248909",
+    "url": "/celebrities-app/static/js/runtime-main.9bb7f6e7.js"
+  }
+]);
